fd386340f3cecdd9322dfc7a159347e04ab5c1e4
Saranga Athukorale <sarangaa@millenniumit.com>
2017-10-31 17:32:10 +0530

    Adding an Icon to the Application
    Signed-off-by: Saranga Athukorale <sarangaa@millenniumit.com>

--- PlantUMLCodeGeneratorGUI/frmMain.Designer.cs
+++ PlantUMLCodeGeneratorGUI/frmMain.Designer.cs
@@ -28,14 +28,15 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.lstFolderList = new System.Windows.Forms.ListBox();
             this.btnAddFolder = new System.Windows.Forms.Button();
             this.btnRemoveFolder = new System.Windows.Forms.Button();
             this.btnGenerate = new System.Windows.Forms.Button();
             this.grpSettings = new System.Windows.Forms.GroupBox();
-            this.chkShowOverriddenMembers = new System.Windows.Forms.CheckBox();
-            this.chkShowPrivateMembers = new System.Windows.Forms.CheckBox();
             this.chkShowProtectedMembers = new System.Windows.Forms.CheckBox();
+            this.chkShowPrivateMembers = new System.Windows.Forms.CheckBox();
+            this.chkShowOverriddenMembers = new System.Windows.Forms.CheckBox();
             this.grpSettings.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,18 +46,19 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lstFolderList.FormattingEnabled = true;
-            this.lstFolderList.Location = new System.Drawing.Point(11, 12);
+            this.lstFolderList.ItemHeight = 15;
+            this.lstFolderList.Location = new System.Drawing.Point(13, 14);
             this.lstFolderList.Name = "lstFolderList";
-            this.lstFolderList.Size = new System.Drawing.Size(260, 212);
+            this.lstFolderList.Size = new System.Drawing.Size(214, 244);
             this.lstFolderList.TabIndex = 0;
             this.lstFolderList.SelectedIndexChanged += new System.EventHandler(this.lstFolderList_SelectedIndexChanged);
             // 
             // btnAddFolder
             // 
             this.btnAddFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnAddFolder.Location = new System.Drawing.Point(281, 12);
+            this.btnAddFolder.Location = new System.Drawing.Point(240, 14);
             this.btnAddFolder.Name = "btnAddFolder";
-            this.btnAddFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFolder.Size = new System.Drawing.Size(87, 27);
             this.btnAddFolder.TabIndex = 1;
             this.btnAddFolder.Text = "Add";
             this.btnAddFolder.UseVisualStyleBackColor = true;
@@ -66,9 +68,9 @@
             // 
             this.btnRemoveFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnRemoveFolder.Enabled = false;
-            this.btnRemoveFolder.Location = new System.Drawing.Point(281, 41);
+            this.btnRemoveFolder.Location = new System.Drawing.Point(240, 47);
             this.btnRemoveFolder.Name = "btnRemoveFolder";
-            this.btnRemoveFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveFolder.Size = new System.Drawing.Size(87, 27);
             this.btnRemoveFolder.TabIndex = 1;
             this.btnRemoveFolder.Text = "Remove";
             this.btnRemoveFolder.UseVisualStyleBackColor = true;
@@ -78,9 +80,9 @@
             // 
             this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.btnGenerate.Enabled = false;
-            this.btnGenerate.Location = new System.Drawing.Point(281, 201);
+            this.btnGenerate.Location = new System.Drawing.Point(240, 232);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.Size = new System.Drawing.Size(87, 27);
             this.btnGenerate.TabIndex = 1;
             this.btnGenerate.Text = "Generate";
             this.btnGenerate.UseVisualStyleBackColor = true;
@@ -93,53 +95,55 @@
             this.grpSettings.Controls.Add(this.chkShowProtectedMembers);
             this.grpSettings.Controls.Add(this.chkShowPrivateMembers);
             this.grpSettings.Controls.Add(this.chkShowOverriddenMembers);
-            this.grpSettings.Location = new System.Drawing.Point(11, 230);
+            this.grpSettings.Location = new System.Drawing.Point(13, 265);
             this.grpSettings.Name = "grpSettings";
-            this.grpSettings.Size = new System.Drawing.Size(343, 89);
+            this.grpSettings.Size = new System.Drawing.Size(312, 103);
             this.grpSettings.TabIndex = 2;
             this.grpSettings.TabStop = false;
             this.grpSettings.Text = "Settings";
             // 
-            // chkShowOverriddenMembers
-            // 
-            this.chkShowOverriddenMembers.AutoSize = true;
-            this.chkShowOverriddenMembers.Location = new System.Drawing.Point(6, 19);
-            this.chkShowOverriddenMembers.Name = "chkShowOverriddenMembers";
-            this.chkShowOverriddenMembers.Size = new System.Drawing.Size(154, 17);
-            this.chkShowOverriddenMembers.TabIndex = 0;
-            this.chkShowOverriddenMembers.Text = "Include Overriden Methods";
-            this.chkShowOverriddenMembers.UseVisualStyleBackColor = true;
-            // 
-            // chkShowPrivateMembers
-            // 
-            this.chkShowPrivateMembers.AutoSize = true;
-            this.chkShowPrivateMembers.Location = new System.Drawing.Point(6, 42);
-            this.chkShowPrivateMembers.Name = "chkShowPrivateMembers";
-            this.chkShowPrivateMembers.Size = new System.Drawing.Size(196, 17);
-            this.chkShowPrivateMembers.TabIndex = 0;
-            this.chkShowPrivateMembers.Text = "Include Private Members && Methods";
-            this.chkShowPrivateMembers.UseVisualStyleBackColor = true;
-            // 
             // chkShowProtectedMembers
             // 
             this.chkShowProtectedMembers.AutoSize = true;
-            this.chkShowProtectedMembers.Location = new System.Drawing.Point(6, 65);
+            this.chkShowProtectedMembers.Location = new System.Drawing.Point(7, 75);
             this.chkShowProtectedMembers.Name = "chkShowProtectedMembers";
-            this.chkShowProtectedMembers.Size = new System.Drawing.Size(209, 17);
+            this.chkShowProtectedMembers.Size = new System.Drawing.Size(235, 19);
             this.chkShowProtectedMembers.TabIndex = 0;
             this.chkShowProtectedMembers.Text = "Include Protected Members && Methods";
             this.chkShowProtectedMembers.UseVisualStyleBackColor = true;
             // 
+            // chkShowPrivateMembers
+            // 
+            this.chkShowPrivateMembers.AutoSize = true;
+            this.chkShowPrivateMembers.Location = new System.Drawing.Point(7, 48);
+            this.chkShowPrivateMembers.Name = "chkShowPrivateMembers";
+            this.chkShowPrivateMembers.Size = new System.Drawing.Size(220, 19);
+            this.chkShowPrivateMembers.TabIndex = 0;
+            this.chkShowPrivateMembers.Text = "Include Private Members && Methods";
+            this.chkShowPrivateMembers.UseVisualStyleBackColor = true;
+            // 
+            // chkShowOverriddenMembers
+            // 
+            this.chkShowOverriddenMembers.AutoSize = true;
+            this.chkShowOverriddenMembers.Location = new System.Drawing.Point(7, 22);
+            this.chkShowOverriddenMembers.Name = "chkShowOverriddenMembers";
+            this.chkShowOverriddenMembers.Size = new System.Drawing.Size(170, 19);
+            this.chkShowOverriddenMembers.TabIndex = 0;
+            this.chkShowOverriddenMembers.Text = "Include Overriden Methods";
+            this.chkShowOverriddenMembers.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(366, 327);
+            this.ClientSize = new System.Drawing.Size(339, 377);
             this.Controls.Add(this.grpSettings);
             this.Controls.Add(this.btnGenerate);
             this.Controls.Add(this.btnRemoveFolder);
             this.Controls.Add(this.btnAddFolder);
             this.Controls.Add(this.lstFolderList);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frmMain";
